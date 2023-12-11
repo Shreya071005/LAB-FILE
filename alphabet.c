1ed0894ddfb1e0df7c9145c350f45b6b699f9b80
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 14:10:00 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,13 @@
+//  WRITE ALL THE ALPHABET FROM A TO Z
+
+#include<stdio.h>
+int main()
+{
+    char c ;
+    printf("the alphabet from a to z");
+    for (c = 'a'; c < ='z'; c++ )
+    {
+        printf("%c",c);
+    }
+    return 0;
+}  
