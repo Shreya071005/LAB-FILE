ff415c41ca88a558ef2ae62538df9e22b251a1ed
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 14:11:38 +0530

    Create natural num.c

---
+++ natural num.c
@@ -0,0 +1,13 @@
+//WRITE ALL NATURAL NUMBER FROM ONE TO N
+
+#include<stdio.h>
+int main()
+{
+    int n , i;
+    printf("number of elements");
+    scanf("%d",&n);
+    for(i=1 ; i<=n ; i++){
+        printf("%d ",i);
+    }
+    return 0;
+}
