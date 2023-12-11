1da50a84e90f828cc79635d511d7bdbbcd41c898
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 13:51:57 +0530

    Create divide.c

---
+++ divide.c
@@ -0,0 +1,16 @@
+//WAP TO DIVIDE TO NUMBER
+#include<stdio.h>
+int main()
+{
+    int a , b;
+    float c;
+    printf("ENTER THE VALUE OF A,B AND C \n");
+    scanf("%d %d %f",&a,&b,&c);
+    printf("the value of a/b is %d \n",a/b);
+    printf("the value of b/a is %d \n", b/a);
+    printf("the value of b/c is %f \n", b/c);
+    printf("the value of b/c is %f \n", c/b);
+    printf("the value of b/a is %f \n", b/a);
+    printf("the value of a/b is %f \n", a/b);
+    return 0;
+}
