4194c96f86463a4d0a14a779a71cbfaf8e2ce8bc
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 14:05:54 +0530

    Create right shift.c

---
+++ right shift.c
@@ -0,0 +1,12 @@
+//USE OF RIGHT SHIFT operator
+
+#include<stdio.h>
+int main()
+{
+    int n , c;
+    printf("enter the number");
+    scanf("%d",&n);
+    c = n>>2;
+    printf("the value of c is: %d",c);
+    return 0;
+}
