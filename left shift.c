4963500b199305c0c9c258b69dbbdb35871a5bdc
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 14:05:02 +0530

    Create left shift.c

---
+++ left shift.c
@@ -0,0 +1,12 @@
+//USE THE LEFT SHIFT operator
+
+#include<stdio.h>
+int main()
+{
+    int n , c;
+    printf("enter the number");
+    scanf("%d",&n);
+    c = n<<2;//using bitwise operator
+    printf("the value of c is %d",c);
+    return 0;
+}
