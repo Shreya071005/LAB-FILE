cc9edc9034c8b85cc807b80c4393d9ae08efa823
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 13:53:56 +0530

    Create multiply.c

---
+++ multiply.c
@@ -0,0 +1,11 @@
+//WAP TO FIND THE MULTIPLY TWO FLOATING NUMBER
+
+#include<stdio.h>
+int main()
+{
+    float a , b;
+    printf("the value of a and b \n");
+    scanf("%f %f",&a , &b);
+    printf("the value of a * b is %f",a * b);
+    return 0;
+}
