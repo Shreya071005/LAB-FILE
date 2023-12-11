0fe693469654b3493fcfddfece88b821d7c08715
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 14:07:23 +0530

    Create post inc and dec.c

---
+++ post inc and dec.c
@@ -0,0 +1,12 @@
+//USE OF POST INCREMENT AND POST DECREMENT
+
+#include<stdio.h>
+int main()
+{
+    int a = 8;
+    printf("the value of a is %d \n",a);
+    printf("the value of a++ is %d \n",a++);//post increment
+    //printf("the value of a-- is %d \n",a--);//post decrement
+    printf("the value of a is %d \n",a);
+    return 0;
+}
