7346593dddef6ea47dbb73bb3d7d1289f41f296d
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 13:57:38 +0530

    Create rect area.c

---
+++ rect area.c
@@ -0,0 +1,15 @@
+//WAP TO FIND THE AREA OF RECTANGLE
+
+#include<stdio.h>
+int main()
+{
+    int l, b;
+    int area;
+    printf("the value of length \n");
+    scanf("%d",&l);
+    printf("the value of bredth \n");
+    scanf("%d",&b);
+    area = l*b;
+    printf("the area of rectangle is %d \n",area);
+    return 0;
+}
