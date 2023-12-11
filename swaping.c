6de15dd817de178d728faafd9afb08d74afe86c0
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 13:56:08 +0530

    Create swaping.c

---
+++ swaping.c
@@ -0,0 +1,16 @@
+//SWAP TWO NUMBER WITHOUT USING THIRD VARIABLE
+
+#include<stdio.h>
+int main()
+{
+    int a , b;
+    //BEFORE SWAPPING
+    printf("the value of a \n");
+    scanf("%d",&a);
+    printf("the value of b \n");
+    scanf("%d",&b);
+    //AFTER SWAPPING
+    printf("the value of new a %d \n",b);
+    printf("the value of new b %d \n",a);
+    return 0;
+}
