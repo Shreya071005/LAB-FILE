808057ff16c57c5ef14df1b43d85fa2da58e1f6d
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 14:30:09 +0530

    Create ASCII with value.c

---
+++ ASCII with value.c
@@ -0,0 +1,11 @@
+//WAP PRINT ALL ASCII CHARACTER  WITH  THEIR VALUE
+
+#include<stdio.h>
+int main()
+{
+    char i;
+    for(i='a' ; i<='z' ; i++){
+        printf("%c = %d \t",i,i);
+    }
+    return 0;
+}
