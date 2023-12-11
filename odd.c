80d18290c78dd94c10f959755cbf5e158cef0e63
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 14:12:56 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,13 @@
+//PRINT  ALL ODD NUMBER
+
+#include<stdio.h>
+int main()
+{
+    int  i;
+    for(i=0 ; i<=100 ; i++){
+        if(i%2 != 0){
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
