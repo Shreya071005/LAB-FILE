b1ba5a0b14cb34983b1299a9e7b2701f3fe3cf1c
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 13:52:56 +0530

    Create ascii.c

---
+++ ascii.c
@@ -0,0 +1,12 @@
+//WAP THE PROGRAM TO FIND ASCII VALUE
+
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("ENTER THE CHARACTER : ");
+    scanf("%c",&c);
+    printf("the value of character is %c = %d" , c,c);
+    return 0;
+
+}
