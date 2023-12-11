cb190a9fcf30e648c7430a4224ccab17e5d162b3
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 14:03:18 +0530

    Create largest number.c

---
+++ largest number.c
@@ -0,0 +1,22 @@
+//WAP TO FIND THE LARGEST NUMBER USING THE LOGICAL 'AND' operator
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("the value of a , b , c \n");
+    scanf("%d %d %d",&a , &b , &c);
+    if(a>b && a>c){
+        printf("a is greatest");
+    }
+    else if(b>a && b>c){
+        printf("b is greatest");
+    }
+    else if(c>a && c>b){
+        printf("c is greatest");
+    }
+    else if(
+            printf("all are equal")
+    )
+        return 0;
+}
