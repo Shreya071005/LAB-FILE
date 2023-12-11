c5c89a3bcabe5e000d73fbf8ab3a63dcf39576bc
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 13:58:29 +0530

    Create sq area.c

---
+++ sq area.c
@@ -0,0 +1,13 @@
+//WAP TO FIND THE AREA OF SQUARE
+
+#include<stdio.h>
+int main()
+{
+    int s;
+    printf("the side of square is \n");
+    scanf("%d",&s);
+    int area;
+    area= s*s;
+    printf("the area of square %d \n",area);
+    return 0;
+}
