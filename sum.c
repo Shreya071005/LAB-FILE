d8394b489e28a63598ab1290ba2e035b7c2aa3bf
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 13:50:23 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+//WAP TO FIND SUM OF TWO NUMBER
+#include<stdio.h>
+int main()
+{
+    int a , b;
+    printf("enter the value of two integer \n");
+    scanf("%d %d",&a ,&b);
+    int sum;
+    sum = a + b;
+    printf("the sum of a+b is %d",sum);
+    return 0;
+}
