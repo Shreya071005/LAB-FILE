d3435d297908d4a89ff61405f96c4ea22785f4e5
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 14:06:40 +0530

    Create inc and dec.c

---
+++ inc and dec.c
@@ -0,0 +1,12 @@
+//print pre increment and pre decrement
+
+#include<stdio.h>
+int main()
+{
+    int a = 6;
+    printf("the value of a is %d \n",a);
+    printf("the value of ++a is %d \n",++a);//pre increment
+    printf("the value of --a is %d \n",--a);//pre decrement
+    printf("the value of a is %d \n",a);//original value of a
+return 0;
+}
