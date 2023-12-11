f2de351409a15b8b98479c1d2e490bafeffe2af9
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 14:26:07 +0530

    Create enter num and print word.c

---
+++ enter num and print word.c
@@ -0,0 +1,11 @@
+//ENTER A NUMBER AND PRINT IT IN WORD
+
+#include<stdio.h>
+int main()
+{
+    int num ;
+    printf("enter the number");
+    scanf("%d",&num);
+    printf("the value of %c = %d",num , num);
+    return 0;
+}
