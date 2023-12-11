cadd324e8a0a213998690d149d1ae61c7a53ec6e
Shreya071005 <153171723+Shreya071005@users.noreply.github.com>
2023-12-11 13:51:12 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,13 @@
+//WAP TO FIND THE AREA OF CIRCLE
+#include<stdio.h>
+int main()
+{
+    float r ;
+    printf("the value of radius is");
+    scanf("%f",&r);
+    float pie = 3.14 ;
+    float area;
+    area = pie * r * r;
+    printf("the area of circle is %f",area);
+    return 0 ;
+}
